9b71eecfeabe33ede9051b37e50ecea8d6fd2729
Bonifazi <renzo.bonifazi@wur.nl>
2025-04-07 19:49:19 +0200

    added level order for labels for age of sires and dams

--- R/make_stacked_lineplot.R
+++ R/make_stacked_lineplot.R
@@ -19,11 +19,12 @@
 #' data <- data.frame(Year = c(2000, 2001, 2002),
 #'                    Group1 = c(10, 20, 15),
 #'                    Group2 = c(5, 10, 8))
-#' make_stacked_lineplot(data)
+#' levels_order <- c("Group1", "Group2")
+#' make_stacked_lineplot(data, levels_order = levels_order)
 #'
 #' @export
 
-make_stacked_lineplot <- function(data, dots = F) {
+make_stacked_lineplot <- function(data, dots = F, levels_order = NULL) {
   if(dots) {
     plot_dots <- geom_point(position = "stack") # size = 2
   } else plot_dots <- NULL
@@ -31,7 +32,10 @@ make_stacked_lineplot <- function(data, dots = F) {
     pivot_longer(cols = -Year,
                  names_to = "group",
                  values_to = "value") %>%
-    ggplot(aes(x = Year, y = value, fill = group)) +
+    # ggplot(aes(x = Year, y = value, fill = group)) +
+    ggplot(aes(x = Year, y = value,
+               fill = factor(group, levels = levels_order))
+           ) +
     geom_area(color = "black")+
     plot_dots +
     scale_x_continuous(
